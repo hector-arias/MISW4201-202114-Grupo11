36f22b2da19207505a1706d04725e92aaa354e73
hector-arias <h.arias@uniandes.edu.co>
2021-09-10 19:34:36 -0500

    Se agrega linea base

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-## MISW4201-202114-Grupo11 
 
-## Reports  
-[GitInspector](https://MISW-4102-ProcesosDeDesarrolloAgil.github.io/MISW4201-202114-Grupo11/reports) 
--- flaskr/config/testing.py
+++
@@ -1,9 +0,0 @@
-# Parámetros para activar el modo debug
-TESTING = True
-DEBUG = True
-
-APP_ENV = 'testing'
-
-WTF_CSRF_ENABLED = False
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///tutorial_canciones_testing.db'
