e031c27ec0cca83637e71b3719485c3dc59710cf
hector-arias <h.arias@uniandes.edu.co>
2021-09-11 22:30:19 -0500

    Se agrega linea base

---
+++ flaskr/config/testing.py
@@ -0,0 +1,9 @@
+# Parámetros para activar el modo debug
+TESTING = True
+DEBUG = True
+
+APP_ENV = 'testing'
+
+WTF_CSRF_ENABLED = False
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///tutorial_canciones_testing.db'
